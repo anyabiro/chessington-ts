1c52d157c58d15e85dc060d1b091450212e1162f
Anya Biro <Anya.Biro@softwire.com>
2025-07-07 12:34:41 +0300

    Added available moves for pawns.

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import player from "../player";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,19 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMoves: Square[] = [];
+        let currentSquare: Square = board.findPiece(this);
+
+        let upSquare: Square = new Square(currentSquare.row + 1, currentSquare.col);
+        if (upSquare.row < 8 && board.getPiece(upSquare) === undefined) {
+            availableMoves.push(upSquare);
+        }
+
+        let downSquare: Square = new Square(currentSquare.row - 1, currentSquare.col);
+        if (downSquare.row >= 0 && board.getPiece(downSquare) === undefined) {
+            availableMoves.push(downSquare);
+        }
+
+        return availableMoves;
     }
 }
