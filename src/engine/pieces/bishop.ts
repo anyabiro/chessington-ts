b51b28a14da334072a865cf17519633d88d71890
Anya Biro <Anya.Biro@softwire.com>
2025-07-07 15:21:37 +0300

    Added bishop available moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,47 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves: Square[] = [];
+        const currentSquare: Square = board.findPiece(this);
+        let nextRow, nextCol: number;
+
+        if (currentSquare.row < currentSquare.col) {
+            nextRow = 0;
+            nextCol = currentSquare.col - currentSquare.row;
+        } else {
+            nextRow = currentSquare.row - currentSquare.col;
+            nextCol = 0;
+        }
+
+        while (1) {
+            let nextSquare: Square = new Square(nextRow, nextCol);
+            if (!nextSquare.isInBounds()) {
+                break;
+            }
+
+            if (board.isEmpty(nextSquare)) {
+                availableMoves.push(nextSquare);
+            }
+            nextRow++;
+            nextCol++;
+        }
+
+        nextRow = 0;
+        nextCol = currentSquare.row + currentSquare.col;
+
+        while (1) {
+            let nextSquare: Square = new Square(nextRow, nextCol);
+            if (!nextSquare.isInBounds()) {
+                break;
+            }
+
+            if (board.isEmpty(nextSquare)) {
+                availableMoves.push(nextSquare);
+            }
+            nextRow++;
+            nextCol--;
+        }
+
+        return availableMoves;
     }
 }
